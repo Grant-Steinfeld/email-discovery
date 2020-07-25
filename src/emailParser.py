90fc772724edeaa74ed2a33af0a56864abe4996a
Grant Steinfeld <grant.steinfeld.tech@gmail.com>
2020-07-25 21:16:42 +0000

    improved parser to pull text of email

--- src/emailParser.py
+++ src/emailParser.py
@@ -1,19 +1,31 @@
 from email.parser import Parser as emp
+from email.message import Message as emm
 from src.errors.UserDefinedErrors import NoTextPartFound
+import pdb
 
 def parse_email(email_text):
     email_msg = emp().parsestr(email_text)
     return email_msg
 
-def parse_email_text(email_text):
+def parse_email_parts(email_text):
     email_text_body = None
     email_msg = parse_email(email_text)
-    #find text portion
 
+    #
+    email_body_text = get_email_body_text(email_msg)
+
+    return { 'body_text': email_body_text }
+
+def get_email_body_text(email_msg):
     payloads = email_msg.get_payload()
+    #usually text and html
+    #
     for payload in payloads:
-        if payload.get_content_type() == 'text/plain':
-            return payload.get_payload()
-        else:
-            raise NoTextPartFound("no text part found")
+        #pdb.set_trace()
+        if type(payload) is emm:
+            if payload.get_content_type() == 'text/plain':
+                return payload.get_payload()
+       
+    return None
+    #raise NoTextPartFound("no text part found")
             
--- tests/unit/test_mailparser.py
+++ tests/unit/test_mailparser.py
@@ -1,12 +1,21 @@
+import os
+import glob
 from src.emailParser import parse_email
-from src.emailParser import parse_email_text
+from src.emailParser import parse_email_parts
 
-mail_file = '/home/developer/neo-postfix-processor/tests/mockdata/mail.samp'
+mock_dir = '/home/developer/neo-postfix-processor/tests/mockdata'
+mail_file = mock_dir + '/mail.samp'
+med_sample_dir = mock_dir + '/med-samp-mail'
 
-def read_file_off_disk(mock_mail_message):
-	with open(mock_mail_message) as emailfile:
+def read_file_off_disk(mail_sample_path):
+	with open(mail_sample_path) as emailfile:
 		data = emailfile.read()
 	return data
+
+def get_list_files(dirpath, pattern='*.txt'):
+	if dirpath.endswith( os.path.sep) == False:
+		dirpath += os.path.sep
+	return glob.glob("{}{}".format(dirpath, pattern))
 
 def test_mockmail():
 	data = read_file_off_disk(mail_file)
@@ -24,10 +33,30 @@ def test_readmail():
 	text = actual.get_payload()[0].get_payload()
 	assert len(text) == 501
 
-def test_read_text_off_mail():
+def test_read_text_off_1_mail_file():
 	data = read_file_off_disk(mail_file)
 	assert len(data) == 1672
-	actual = parse_email_text(data)
-	assert len(actual) == 501
+	actual = parse_email_parts(data)
+	assert len(actual['body_text']) == 501
 	#** Confirm your subscription to the NI mate newsletter
-	assert actual[:54] == '** Confirm your subscription to the NI mate newsletter'
+	assert actual['body_text'][:54] == '** Confirm your subscription to the NI mate newsletter'
+
+
+def test_get_med_samp_mails():
+	mail_samples_list = get_list_files(med_sample_dir)
+	assert len(mail_samples_list) == 33
+	for mail_path in mail_samples_list:
+		email_text = read_file_off_disk(mail_path)
+		email_text_parts = parse_email_parts(email_text)
+
+	
+		assert type(email_text_parts) is dict	
+
+
+
+def test_get_mail_paths():
+	actual = get_list_files(med_sample_dir)
+	assert type(actual) is list
+	assert len(actual) == 33
+
+
