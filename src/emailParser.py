af725ac3182911995f41f6305482433469ee95d5
Grant Steinfeld <grant.steinfeld.tech@gmail.com>
2020-07-31 18:08:57 +0000

    refinements due to mentorship session  with MP

--- src/emailParser.py
+++ src/emailParser.py
@@ -1,61 +1,27 @@
 from email.parser import Parser as emp
 from email.message import Message as emm
+from email import policy
 from src.errors.UserDefinedErrors import NoTextPartFound, EmptyEmail
 
 def parse_email(email_text):
-    email_msg = emp().parsestr(email_text)
+    email_msg = emp(policy=policy.default).parsestr(email_text)
     return email_msg
 
 def parse_email_parts(email_text):
-
-    if (len(email_text.strip()) == 0):
-        raise EmptyEmail("empty email")
-
-    email_text_body = None
-    email_msg = parse_email(email_text)
-
-    email_body_text = get_email_body_text(email_msg)
+    email_msg = parse_email(email_text)  #legacy parser
+    
+    email_body_part = email_msg.get_body(('related', 'plain'))
+    email_body_text = email_body_part and email_body_part.get_content()
     from_ = email_msg['from']
 
     return { 
         'body_text': email_body_text ,
         'subject': email_msg['subject'],
-        'from_raw': from_,
-        'from_name': get_email_address_name_utf8(from_),
-        'from': get_just_email_address(from_)}
-
-def get_email_body_text(email_msg):
-    payloads = email_msg.get_payload()
-    #usually text and html
-    #
-    for payload in payloads:
-    
-        if type(payload) is emm:
-            if payload.get_content_type() == 'text/plain':
-                return payload.get_payload()
-       
-    return None
-    #raise NoTextPartFound("no text part found")
-
-def get_just_email_address(address):
-    
-    plain_email = address
-    first_angle_bracket = plain_email.find('<')
-    if first_angle_bracket != -1:
-        second_angle_bracket = plain_email.find('>')
-        return plain_email[first_angle_bracket+1:second_angle_bracket]
-    return plain_email
-
-def get_email_address_name_utf8(address):
-    address_name = ''
-    #=?utf-8?Q?NI=20mate=20Newsletter?=
-    utf8prefix = '=?utf-8?Q?'
-    utf8suffix = '?='
-    suffixIndex = address.find(utf8suffix)
+        'message-id': email_msg['Message-Id'],
+        
+        'from_name': from_.addresses[0].display_name,
+        'from':  from_.addresses[0].addr_spec
+    }
 
 
-    if address.startswith(utf8prefix):
-        address_name = address[len(utf8prefix) : suffixIndex]
-        #hack replace =20 (spaces)
-        address_name = address_name.replace('=20',' ')
-    return address_name
+
--- tests/unit/test_mailparser.py
+++ tests/unit/test_mailparser.py
@@ -3,7 +3,9 @@ import glob
 from src.emailParser import parse_email
 from src.emailParser import parse_email_parts
 
-mock_dir = '/home/developer/neo-postfix-processor/tests/mockdata'
+home_dir = '/home/developer/neo-postfix-processor'
+mock_dir = home_dir + '/tests/mockdata'
+out_dir = home_dir + '/tests/output'
 mail_file = mock_dir + '/mail.samp'
 med_sample_dir = mock_dir + '/med-samp-mail'
 
@@ -39,37 +41,45 @@ def test_read_text_off_1_mail_file():
 	actual = parse_email_parts(data)
 	assert len(actual['body_text']) == 501
 	assert len(actual['subject']) == 47
+	assert len(actual['message-id']) == 67
 
 	assert actual['subject'] == 'NI mate Newsletter: Please Confirm Subscription'
 	assert actual['body_text'][:54] == '** Confirm your subscription to the NI mate newsletter'
 
 
-	assert len(actual['from_raw']) == 53
+	#assert len(actual['from_raw']) == 53
 	assert len(actual['from']) == 16
 	assert len(actual['from_name']) == 18
 
-
-	assert actual['from_raw'] == '=?utf-8?Q?NI=20mate=20Newsletter?= <info@delicode.fi>'
+	assert actual['message-id'] == '<9656357.20180313131359.5aa7ce976a5fb2.44907804@mail7.mcsignup.com>'
+	#assert actual['from_raw'] == '=?utf-8?Q?NI=20mate=20Newsletter?= <info@delicode.fi>'
 	assert actual['from'] == 'info@delicode.fi'
 	assert actual['from_name'] == 'NI mate Newsletter'
 
 def test_get_med_samp_mails():
 	mail_samples_list = get_list_files(med_sample_dir)
 	assert len(mail_samples_list) == 33
-	for mail_path in mail_samples_list:
-		try:
-			email_text = read_file_off_disk(mail_path)
-			email_text_parts = parse_email_parts(email_text)
-
-			assert type(email_text_parts) is dict	
-			assert 'subject' in email_text_parts
-			assert 'body_text' in email_text_parts
-			assert 'from' in email_text_parts
-		
-		except Exception as parsex:
+	for i, mail_path in enumerate(mail_samples_list):
+		email_text = read_file_off_disk(mail_path)
+		if not email_text.strip():
 			continue
+		email_text_parts = parse_email_parts(email_text)
+
+		assert type(email_text_parts) is dict	
+		assert 'subject' in email_text_parts
+		assert 'body_text' in email_text_parts
+		if email_text_parts.get('body_text') is not None:
+			write_file_text(i, email_text_parts['body_text'])
+		
+		assert 'from' in email_text_parts
+	
 
 
+
+def write_file_text(id, data, path_=out_dir):
+	path_ = f'{path_}/{id}_body.txt'
+	with open(path_,'w') as nf:
+		nf.write(data)
 
 
 def test_get_mail_paths():
