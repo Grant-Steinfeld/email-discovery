2568839f9934a65b64ff42c3c08bfd29923ed502
Grant Steinfeld <46547925+Grant-Steinfeld@users.noreply.github.com>
2020-08-05 05:07:32 -0400

    Flask REST API added

--- src/main.py
+++ src/main.py
@@ -1,15 +1,42 @@
-import logging
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+from emailParser import mail_parser
+
+app = Flask(__name__)
+api = Api(
+    app,
+    version="3.0.0",
+    title="Emai parser Service",
+    description="This is a microservice that extracts email part for the Breaking News Project",
+)
+
+emailNS = api.namespace(
+    "Email",
+    description="Operations associated with email parsing",
+)
 
 
-def main():
-    """  testing logger """
-    logger.info("starting")
+emailMesage = api.model(
+    "emailMesage",
+    {
+        "emailText": fields.String(
+            required=True, description="full raw email including headers and boundries"
+        ),
+    },
+)
+
+@emailNS.route("/parse")
+@emailNS.response(404, "Invalid email")
+class ParseEmail(Resource):
+    @currencyNS.doc("parse_email_meta")
+    @currencyNS.expect(emailMesage)
+    @currencyNS.marshal_with(emailMesage, code=201)
+    def post(self):
+        if "emailText" in api.payload:
+            return mail_parse(api.payload["emailText"])
+        else:
+            api.abort(400, "pass in email as raw text)
 
 
 if __name__ == "__main__":
-    """ this should be in the program's main/start/run function """
-    import logging.config
-
-    logging.config.fileConfig("logging.conf")
-    logger = logging.getLogger(__name__)
-    main()
+    app.run(host="0.0.0.0", debug=True, port=8808)
